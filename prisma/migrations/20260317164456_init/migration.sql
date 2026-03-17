9fdb137b12caed3463b474b18b661e7ac646ebe6
Sarthak Chaturvedi <sarthakchat2018@gmail.com>
2026-03-17 22:17:59 +0530

    Scaffolded the project

---
+++ prisma/migrations/20260317164456_init/migration.sql
@@ -0,0 +1,49 @@
+-- CreateEnum
+CREATE TYPE "VisistedUrlStatus" AS ENUM ('PENDING', 'VISITED', 'FAILED');
+
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "links" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "InvertedIndex" (
+    "id" TEXT NOT NULL,
+    "word" TEXT NOT NULL,
+    "documentId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "InvertedIndex_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VisitedUrls" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "status" "VisistedUrlStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "VisitedUrls_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Document_url_key" ON "Document"("url");
+
+-- CreateIndex
+CREATE INDEX "InvertedIndex_word_idx" ON "InvertedIndex"("word");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VisitedUrls_url_key" ON "VisitedUrls"("url");
+
+-- CreateIndex
+CREATE INDEX "VisitedUrls_url_idx" ON "VisitedUrls"("url");
+
+-- AddForeignKey
+ALTER TABLE "InvertedIndex" ADD CONSTRAINT "InvertedIndex_documentId_fkey" FOREIGN KEY ("documentId") REFERENCES "Document"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
